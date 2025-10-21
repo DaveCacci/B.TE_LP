fdf7b742dd2139164317ab1136ef9398602ed8e2
Davide Carecci <davide.carecci@polimi.it>
2025-10-21 16:31:57 +0200

    Continue to review code
    Deterministic, uncertainty and robust problem cells

--- Project/my_utilities.py
+++ Project/my_utilities.py
@@ -14,6 +14,9 @@ def flatten_list(nested_list):
     return flattened_list
 
 def save_list_to_excel(nested_list, file_path, sheet_name):
+    ''' Save a nested list to an Excel file.
+    If the specified sheet already exists, append the data to the next available column.
+    If the sheet does not exist, create a new sheet and write the data there.'''
 
     # Flatten the nested list
     flattened_list = flatten_list(nested_list)
@@ -41,11 +44,58 @@ def save_list_to_excel(nested_list, file_path, sheet_name):
 
     # Save the workbook back to the file
     workbook.save(file_path)
+# ------------------------------------------------------------------------------------------------ #
 
 # DECLARE THE FUNCTION TO READ DATA FROM EXCEL FILES
 import pandas as pd
 def read_excel_file(file_path):
-    # Read the Excel file and return a dictionary of DataFrames for each sheet
+    ''' Read an Excel file and return a dictionary of DataFrames for each sheet. '''
     xls = pd.ExcelFile(file_path)
     sheet_dict = {sheet_name: pd.read_excel(xls, sheet_name, skiprows=[1]) for sheet_name in xls.sheet_names}
     return sheet_dict
+# ------------------------------------------------------------------------------------------------ #
+
+# DECLARE THE FUNCTION TO RANDOMLY SAMPLE FROM A BETA DISTRIBUTION
+'''This function is randomly sampling from a Beta distribution 
+    that has been scaled and shifted to fit within a specified range [min, max].
+    vector is a list or array-like structure containing three elements:
+    - mean: The desired mean of the distribution.
+    - max: The maximum value of the distribution.
+    - min: The minimum value of the distribution.'''
+import numpy as np
+def beta_sampler(vector):
+  mean = vector[0]
+  max = vector[1]
+  min = vector[2]
+
+  alpha = max-min
+  beta = alpha*(max-min)/(mean-min)-alpha
+  return (max-min)*np.random.beta(alpha,beta)+min
+# ------------------------------------------------------------------------------------------------ #
+
+# Declare function to generate costs data for one year in an incremental way
+'''This function generates a time series of costs for a feedstock over one year (52 weeks).
+The costs are generated in an incremental manner, starting from an initial cost sampled from a Beta distribution.
+The cost can fluctuate weekly based on a triangular distribution, but it is constrained within specified minimum and maximum bounds.
+vector is a list or array-like structure containing three elements:
+- mean: The desired mean of the distribution.
+- max: The maximum value of the distribution.
+- min: The minimum value of the distribution.'''
+import matplotlib.pyplot as plt
+def generate_cost_series(vector, number_of_points, plot=False):
+  min = vector[2]
+  max= vector[1]
+  cost_year = np.zeros(number_of_points)
+  cost_year[0] = beta_sampler(vector)
+  for i in range(cost_year.shape[0]-1):
+    incremented= cost_year[i]+np.random.triangular(-0.5,0,0.5)
+    if incremented <=min:
+      cost_year[i+1] = min
+    elif incremented >=max:
+      cost_year[i+1] = max
+    else:
+      cost_year[i+1] = incremented
+
+  if plot:
+    plt.plot(np.arange(1,len(cost_year)+1),cost_year)
+  return cost_year
